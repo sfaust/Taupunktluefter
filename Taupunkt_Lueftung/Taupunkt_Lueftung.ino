99de3af80c706ffd3bab49f327a76afc31c8b07a
Simon Faust <simon.faust@gmx.de>
2024-01-27 19:37:27 +0100

    renamed vars, sensors

--- Taupunkt_Lueftung/Taupunkt_Lueftung.ino
+++ Taupunkt_Lueftung/Taupunkt_Lueftung.ino
@@ -28,8 +28,8 @@ bool rel;
 #define TEMP1_min  10.0 // Minimale Innentemperatur, bei der die Lüftung aktiviert wird
 #define TEMP2_min -10.0 // Minimale Außentemperatur, bei der die Lüftung aktiviert wird
 
-DHT dht1(DHTPIN_1, DHTTYPE_1); //Der Innensensor wird ab jetzt mit dht1 angesprochen
-DHT dht2(DHTPIN_2, DHTTYPE_2); //Der Außensensor wird ab jetzt mit dht2 angesprochen
+DHT dhti(DHTPIN_1, DHTTYPE_1); //Der Innensensor wird ab jetzt mit dhti angesprochen
+DHT dhta(DHTPIN_2, DHTTYPE_2); //Der Außensensor wird ab jetzt mit dhta angesprochen
 
 LiquidCrystal_I2C lcd(0x27,20,4); // LCD: I2C-Addresse und Displaygröße setzen
 
@@ -55,45 +55,45 @@ void setup() {
   byte Strich[8] = {B00100,B00100,B00100,B00100,B00100,B00100,B00100,B00100};   // Sonderzeichen senkrechter Strich definieren
   lcd.createChar(1, Strich);
     
-  dht1.begin(); // Sensoren starten
-  dht2.begin();   
+  dhti.begin(); // Sensoren starten
+  dhta.begin();   
 }
 
 void loop() {
 
-  float h1 = dht1.readHumidity()+Korrektur_h_1;       // Innenluftfeuchtigkeit auslesen und unter „h1“ speichern
-  float t1 = dht1.readTemperature()+ Korrektur_t_1;   // Innentemperatur auslesen und unter „t1“ speichern
-  float h2 = dht2.readHumidity()+Korrektur_h_2;       // Außenluftfeuchtigkeit auslesen und unter „h2“ speichern
-  float t2 = dht2.readTemperature()+ Korrektur_t_2;   // Außentemperatur auslesen und unter „t2“ speichern
+  float hi = dhti.readHumidity()+Korrektur_h_1;       // Innenluftfeuchtigkeit auslesen und unter „hi“ speichern
+  float ti = dhti.readTemperature()+ Korrektur_t_1;   // Innentemperatur auslesen und unter ti speichern
+  float ha = dhta.readHumidity()+Korrektur_h_2;       // Außenluftfeuchtigkeit auslesen und unter ha speichern
+  float ta = dhta.readTemperature()+ Korrektur_t_2;   // Außentemperatur auslesen und unter ta speichern
   
   if (fehler == true)  // Prüfen, ob gültige Werte von den Sensoren kommen
   {
     fehler = false; 
-    if (isnan(h1) || isnan(t1) || h1 > 100 || h1 < 1 || t1 < -40 || t1 > 80 )  {
+    if (isnan(hi) || isnan(ti) || hi > 100 || hi < 1 || ti < -40 || ti > 80 )  {
       Serial.println(F("Fehler beim Auslesen vom 1. Sensor!"));
       lcd.setCursor(0,1);
-      lcd.print(F("Fehler Sensor 1"));
+      lcd.print(F("Fehler Sensor I"));
       fehler = true;
     }else {
      lcd.setCursor(0,1);
-     lcd.print(F("Sensor 1 in Ordnung"));
+     lcd.print(F("Sensor I in Ordnung"));
    }
   
     delay(2000);  // Zeit um das Display zu lesen
   
-      if (isnan(h2) || isnan(t2) || h2 > 100 || h2 < 1 || t2 < -40 || t2  > 80)  {
+      if (isnan(ha) || isnan(ta) || ha > 100 || ha < 1 || ta < -40 || ta  > 80)  {
         Serial.println(F("Fehler beim Auslesen vom 2. Sensor!"));
         lcd.setCursor(0,2);
-        lcd.print(F("Fehler Sensor 2"));
+        lcd.print(F("Fehler Sensor A"));
         fehler = true;
       } else {
         lcd.setCursor(0,2);
-        lcd.print(F("Sensor 2 in Ordnung"));
+        lcd.print(F("Sensor A in Ordnung"));
      }
 
     delay(2000);  // Zeit um das Display zu lesen
   }
-  if (isnan(h1) || isnan(t1) || isnan(h2) || isnan(t2)) fehler = true;
+  if (isnan(hi) || isnan(ti) || isnan(ha) || isnan(ta)) fehler = true;
    
  if (fehler == true) {
     digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten 
@@ -104,28 +104,28 @@ void loop() {
  esp_task_wdt_reset();  // Watchdog zurücksetzen
 
 //**** Taupunkte errechnen********
-float Taupunkt_1 = taupunkt(t1, h1);
-float Taupunkt_2 = taupunkt(t2, h2);
+float Taupunkt_i = taupunkt(ti, hi);
+float Taupunkt_a = taupunkt(ta, ha);
 
 // Werteausgabe auf Serial Monitor
- Serial.print(F("Sensor-1: " ));
+ Serial.print(F("Innen: " ));
   Serial.print(F("Luftfeuchtigkeit: "));
-  Serial.print(h1);                     
+  Serial.print(hi);                     
   Serial.print(F("%  Temperatur: "));
-  Serial.print(t1);
+  Serial.print(ti);
   Serial.print(F("°C  "));
   Serial.print(F("  Taupunkt: "));
-  Serial.print(Taupunkt_1);
+  Serial.print(Taupunkt_i);
   Serial.println(F("°C  "));
 
-  Serial.print("Sensor-2: " );
+  Serial.print("Aussen: " );
   Serial.print(F("Luftfeuchtigkeit: "));
-  Serial.print(h2);
+  Serial.print(ha);
   Serial.print(F("%  Temperatur: "));
-  Serial.print(t2);
+  Serial.print(ta);
   Serial.print(F("°C "));
   Serial.print(F("   Taupunkt: "));
-  Serial.print(Taupunkt_2);
+  Serial.print(Taupunkt_a);
   Serial.println(F("°C  "));
 
 
@@ -134,32 +134,32 @@ float Taupunkt_2 = taupunkt(t2, h2);
   // Werteausgabe auf dem I2C-Display
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print(F("S1: "));
-  lcd.print(t1); 
+  lcd.print(F("SI: "));
+  lcd.print(ti); 
   lcd.write((uint8_t)0); // Sonderzeichen °C
   lcd.write(('C'));
   lcd.write((uint8_t)1); // Sonderzeichen |
-  lcd.print(h1);
+  lcd.print(hi);
   lcd.print(F(" %"));
 
   lcd.setCursor(0,1);
-  lcd.print(F("S2: "));
-  lcd.print(t2); 
+  lcd.print(F("SA: "));
+  lcd.print(ta); 
   lcd.write((uint8_t)0); // Sonderzeichen °C
   lcd.write(('C'));
   lcd.write((uint8_t)1); // Sonderzeichen |
-  lcd.print(h2);
+  lcd.print(ha);
   lcd.print(F(" %"));
 
   lcd.setCursor(0,2);
-  lcd.print(F("Taupunkt 1: "));
-  lcd.print(Taupunkt_1); 
+  lcd.print(F("Taupunkt I: "));
+  lcd.print(Taupunkt_i); 
   lcd.write((uint8_t)0); // Sonderzeichen °C
   lcd.write(('C'));
 
   lcd.setCursor(0,3);
-  lcd.print(F("Taupunkt 2: "));
-  lcd.print(Taupunkt_2); 
+  lcd.print(F("Taupunkt A: "));
+  lcd.print(Taupunkt_a); 
   lcd.write((uint8_t)0); // Sonderzeichen °C
   lcd.write(('C'));
 
@@ -169,12 +169,12 @@ esp_task_wdt_reset(); // Watchdog zurücksetzen
   lcd.clear();
   lcd.setCursor(0,0);
   
-float DeltaTP = Taupunkt_1 - Taupunkt_2;
+float DeltaTP = Taupunkt_i - Taupunkt_a;
 
 if (DeltaTP > (SCHALTmin + HYSTERESE))rel = true;
 if (DeltaTP < (SCHALTmin))rel = false;
-if (t1 < TEMP1_min )rel = false;
-if (t2 < TEMP2_min )rel = false;
+if (ti < TEMP1_min )rel = false;
+if (ta < TEMP2_min )rel = false;
 
 if (rel == true)
 {
